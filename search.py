2882844ec2a20c3a7b3f5485cbe3a04d3b2b8d11
shof0322 <69450534+shof0322@users.noreply.github.com>
2020-11-01 18:07:36 +0900

    task1
    １．入力したワードで、リストを検索して結果をPrint文で表示してみましょう
    ２．１に追加して結果が存在した場合と、しなかった場合で表示する文言が変わるようにしてみましょう
    ３．２に追加して結果が存在しなかった場合に、リストに追加できるようにしてみましょう
    ４．３に追加してリストをCSVから読み込んで登録できるようにしてみましょう

--- search.py
+++ search.py
@@ -1,17 +1,30 @@
 
 ### 検索ツールサンプル
 ### これをベースに課題の内容を追記してください
-
+import csv
+import pprint
 # 検索ソース
-source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
+# source = ["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
+with open('source.csv') as f:
+    # print(f.read())
+    char_name = csv.reader(f)
+    for i in char_name:
+        source = i
+    print(source)
 
 ### 検索ツール
 def search():
-    word =input("鬼滅の登場人物の名前を入力してください >>> ")
+    word = input("鬼滅の登場人物の名前を入力してください >>> ")
     
     ### ここに検索ロジックを書く
-
-    print("{}が見つかりした".format(word))
+    for i in range(len(source)):
+        if source[i] == word:
+            return print("{}が見つかりました".format(word))
+    print("{}は見つかりませんでした".format(word))
+    add_list_flag = input("新しく追加しますか？Y/N >>> ")
+    if add_list_flag == "Y":
+        source.append(word)
+        print(source)
 
 if __name__ == "__main__":
     search()
