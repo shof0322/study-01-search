fea42e090cac6bfe33aadef891a646e5e54775ac
shof0322 <69450534+shof0322@users.noreply.github.com>
2020-11-04 19:22:46 +0900

    Update search.py
    for文削除

--- search.py
+++ search.py
@@ -13,17 +13,16 @@ def search():
     word = input("鬼滅の登場人物の名前を入力してください >>> ")
     
     ### ここに検索ロジックを書く
-    for i in range(len(source)):
-        if word in source:
-            return print("{}が見つかりました".format(word))
+    if word in source:
+        return print("{}が見つかりました".format(word))
+    else:
+        print("{}は見つかりませんでした".format(word))
+        add_list_flag = input("新しく追加しますか？Y/N >>> ")
+        if add_list_flag == "Y":
+            source.append(word)
+            return print(source)
         else:
-            print("{}は見つかりませんでした".format(word))
-            add_list_flag = input("新しく追加しますか？Y/N >>> ")
-            if add_list_flag == "Y":
-                source.append(word)
-                return print(source)
-            else:
-                return print(source)
+            return print(source)
 
 if __name__ == "__main__":
     search()
